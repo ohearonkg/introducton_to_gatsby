bf27185f473f188dcab20e9ac343308d6fcbce2d
sarah-morash <sarah.morash@outlook.com>
2020-04-19 22:11:29 -0300

    continuing work on presentation.

---
+++ customTheme.js
@@ -0,0 +1,16 @@
+export default {
+  styles: {
+    h1: {
+      textTransform: "uppercase",
+    },
+  },
+  fonts: {
+    heading: '"Red Hat Text", sans-serif',
+    body: '"Red Hat Display", sans-serif',
+  },
+  colors: {
+    text: "black",
+    background: "#efefef",
+    primary: "#8a4baf",
+  },
+};
