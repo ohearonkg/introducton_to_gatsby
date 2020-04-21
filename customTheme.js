2ccd0511014ec97d1be6683e5f579494473ac743
sarah-morash <sarah.morash@outlook.com>
2020-04-21 14:17:12 -0300

    almost complete.

--- customTheme.js
+++ customTheme.js
@@ -2,15 +2,24 @@ export default {
   styles: {
     h1: {
       textTransform: "uppercase",
+      fontSize: 3,
+    },
+    p: {
+      fontSize: 0,
+      textAlign: "center",
+    },
+    img: {
+      height: "auto",
     },
   },
   fonts: {
     heading: '"Red Hat Text", sans-serif',
     body: '"Red Hat Display", sans-serif',
   },
+  fontSizes: ["1.333em", "1.777em", "2.369em", "3.157em", "4.209em"],
   colors: {
     text: "black",
-    background: "#efefef",
-    primary: "#8a4baf",
+    background: "#F7FAFC",
+    primary: "#6B46C1",
   },
 };
